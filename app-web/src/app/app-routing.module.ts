ea43916dc95e39e6630e67f507dc388756995211
Suparender <gamesrodrigo094@gmail.com>
2023-05-17 16:55:25 -0300

    componentes

--- app-web/src/app/app-routing.module.ts
+++ app-web/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// importando componentes
+import { HomeComponent } from './page/home/home.component';
+import { ContatoComponent } from './page/contato/contato.component';
+import { SobreComponent } from './page/sobre/sobre.component';
+import { MenuComponent } from './page/menu/menu.component';
+HomeComponent
+ContatoComponent
+SobreComponent
+MenuComponent
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'menu', component:MenuComponent },
+  { path: 'sobre', component:SobreComponent },
+  { path: 'contato', component:ContatoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
